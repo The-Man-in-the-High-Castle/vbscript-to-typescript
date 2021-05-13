8a4975b84787e08f86de3f0c105a8cee9cfda9bf
immament <immament@gmail.com>
2021-05-13 12:17:12 +0200

    feat: convert classes

--- source/cli.ts
+++ source/cli.ts
@@ -1,12 +1,11 @@
-import { fstat, existsSync } from "fs";
-import { convert, convertFile } from "./converter";
+import { existsSync } from "fs";
+import { convertFile } from "./converter";
 
 const file = process.argv[2];
 
 if (existsSync(file)) {
-    console.log(`Processing "${file}"...`)
-    convertFile(file);
-}
-else {
-    console.error(`"${file}" not found!`)
+  console.log(`Processing "${file}"...`);
+  convertFile(file);
+} else {
+  console.error(`"${file}" not found! ${process.cwd()}`);
 }
--- source/converter.ts
+++ source/converter.ts
@@ -1,184 +1,314 @@
 import { readFileSync, writeFileSync } from "fs";
-import * as path from 'path';
+import * as path from "path";
 
 export function convertFile(file: string) {
-    const extension = path.extname(file);
-    const withoutExtension = file.substr(0, file.length - extension.length);
-    const targetFile = `${withoutExtension}.ts`;
-    const baseName = path.basename(file, extension);
+  const extension = path.extname(file);
+  const withoutExtension = file.substr(0, file.length - extension.length);
+  const targetFile = `${withoutExtension}.ts`;
+  const baseName = path.basename(file, extension);
 
-    const content = readFileSync(file, 'utf8');
-    const result = convert(content, baseName);
+  const content = readFileSync(file, "utf8");
+  const result = convert(content, baseName);
 
-    console.log(`Writing to "${targetFile}"...`)
-    writeFileSync(targetFile, result);
+  console.log(`Writing to "${targetFile}"...`);
+  writeFileSync(targetFile, result);
 }
 
-export function convert(input: string, name: string) {
-    let result = convertImports(input, name);
-
-    return result;
+function convert(input: string, name: string) {
+  return convertImports(input, name);
 }
 
-export function convertImports(input: string, name: string) {
-    const items = [] as { name: string, path: string }[];
-    let result = input.replace(/<!-- #include file="(.*?\/)?(.*?).asp" -->/g, (input, group1, group2) => {
-        const path = group1 || './';
-        const file = `${path}${group2}`;
-        items.push({ name: group2, path: file });
-        return `<%\n${group2}();\n%>`;
-    });
-
-    result = convertCode(result);
-    result = convertExpressions(result);
-    result = convertStrings(result);
-
-    result = `\nexport function ${name}() {\n${result}\n}`
-
-    for (const item of items) {
-        result = `import {${item.name}} from "${item.path}"\n${result}`;
+function convertImports(input: string, name: string) {
+  const items = [] as { name: string; path: string }[];
+  let result = input.replace(
+    /<!-- #include file="(.*?\/)?(.*?).asp" -->/gi,
+    (input, group1, group2) => {
+      const path = group1 || "./";
+      const file = `${path}${group2}`;
+      items.push({ name: group2, path: file });
+      return `<%\n${group2}();\n%>`;
     }
+  );
 
-    return result;
+  result = convertCode(result);
+  result = convertExpressions(result);
+  result = convertStrings(result);
+
+  //   result = `\nfunction ${name}() {\n${result}\n}`;
+
+  for (const item of items) {
+    result = `import {${item.name}} from "${item.path}"\n${result}`;
+  }
+
+  return result;
 }
 
-export function convertCode(input: string) {
-    const result = input.replace(/<%([^=][\s\S]*?)%>/g, (input, group1) => {
-        let code = group1;
-        code = convertComments(code);
-        code = convertIfStatements(code);
-        code = convertSwitchStatements(code);
-        code = convertFunctions(code);
-        code = convertForStatements(code);
-        code = convertLoops(code);
-        code = convertSplit(code);
-        code = convertPRec(code);
-        code = convertPLan(code);
+function convertCode(input: string) {
+  let code = convertMany(input);
+  code = convertClass(code);
 
-        return `<%${code}%>`;
-    });
+  code = code.replace(/<%([^=][\s\S]*?)%>/gi, (input, group1) => {
+    let code = convertVbsCode(group1);
 
-    return result;
+    return `<%${code}%>`;
+  });
+
+  return code;
 }
 
-export function convertExpressions(input: string) {
-    const result = input.replace(/<%=([\s\S]*?)%>/g, (input, group1) => {
-        let content = convertPRec(group1);
-        content = convertPLan(content);
+function convertVbsCode(input: string) {
+  let code = input;
 
-        return "${" + content + "}";
-    });
+  code = convertComments(code);
+  code = convertVariables(code);
+  code = convertSwitchStatements(code);
+  code = convertIfStatements(code);
+  code = convertFunctions(code, false);
+  code = convertForStatements(code);
+  code = convertLoops(code);
+  code = convertSplit(code);
+  code = convertPRec(code);
+  code = convertPLan(code);
 
-    return result;
+  return code;
 }
 
-export function convertStrings(input: string) {
-    let result = input.replace(/%>([\s\S]+?)<%/g, "\nResponse.Write(`$1`);\n");
+function convertClassContent(input: string) {
+  let code = input;
+  code = convertProperties(code);
+  code = convertFunctions(code, true);
 
-    // Entire document is a string
-    if (result.indexOf("<%") === -1) {
-        result = `Response.Write(\`${result}\`);`;
+  return code;
+}
+
+function convertClass(input: string) {
+  let result = input.replace(
+    /Class\s+(\w+)\s*(.+?)End\s+Class\s*$/gims,
+    (input, name, content) => {
+      //   const condition = (group1);
+      content = convertClassContent(content);
+      content = convertClassFields(content);
+      return `class ${name}{\n\t${content}\n}`;
     }
+  );
 
-    // Start of the document is a string
-    const firstIndex = result.indexOf("<%");
-    if (firstIndex > 0) {
-        result = `Response.Write(\`${result.substr(0, firstIndex)}\`);\n${result.substring(firstIndex + 2)}`;
+  const regex = /Class\s+(\w+)\s*$(.+?)End\s+Class\s*$/gims;
+  return result;
+}
+
+function convertExpressions(input: string) {
+  const result = input.replace(/<%=([\s\S]*?)%>/gi, (input, group1) => {
+    let content = convertPRec(group1);
+    content = convertPLan(content);
+
+    return "${" + content + "}";
+  });
+
+  return result;
+}
+
+function convertStrings(input: string) {
+  let result = input.replace(/%>([\s\S]+?)<%/gi, "\nResponse.Write(`$1`);\n");
+
+  // Entire document is a string
+  if (result.indexOf("<%") === -1) {
+    result = `Response.Write(\`${result}\`);`;
+  }
+
+  // Start of the document is a string
+  const firstIndex = result.indexOf("<%");
+  if (firstIndex > 0) {
+    result = `Response.Write(\`${result.substr(
+      0,
+      firstIndex
+    )}\`);\n${result.substring(firstIndex + 2)}`;
+  }
+
+  result = result.replace(/%>$/, "");
+
+  // End of the document is a string
+  const lastIndex = result.lastIndexOf("%>");
+  if (lastIndex > -1 && lastIndex < result.length - 2) {
+    result = `${result.substr(0, lastIndex)}\nResponse.Write(\`${result.substr(
+      lastIndex + 3
+    )}\`);`;
+  }
+
+  result = result.replace(/^<%/, "");
+
+  return result;
+}
+
+function convertComments(input: string) {
+  let result = "";
+  const splitted = input.split(/(".*")/gm);
+  for (const part of splitted) {
+    if (part.indexOf(`"`) === 0) {
+      result += part;
+    } else {
+      result += part.replace(/'/gi, "//");
     }
+  }
 
-    result = result.replace(/%>$/, "");
-
-    // End of the document is a string
-    const lastIndex = result.lastIndexOf("%>");
-    if (lastIndex > -1 && lastIndex < result.length - 2) {
-        result = `${result.substr(0, lastIndex)}\nResponse.Write(\`${result.substr(lastIndex + 3)}\`);`;
-    }
-
-    result = result.replace(/^<%/, "");
-
-    return result;
+  return result;
 }
 
-export function convertComments(input: string) {
-    let result = '';
-    const splitted = input.split(/(".*")/gm);
-    for (const part of splitted) {
-        if (part.indexOf(`"`) === 0) {
-            result += part;
-        }
-        else {
-            result += part.replace(/'/g, "//");
-        }
-    }
+function convertIfStatements(input: string) {
+  let result = input.replace(/if +(.*?) +then/gi, (input, group1) => {
+    const condition = convertConditions(group1);
+    return `\nif (${condition}) {\n`;
+  });
+  result = result.replace(/end if/gi, "\n}\n");
+  result = result.replace(/else(?!{)/gi, "\n}\nelse {\n");
 
-    return result;
+  return result;
 }
 
-export function convertIfStatements(input: string) {
-    let result = input.replace(/if +(.*?) +then/g, (input, group1) => {
-        const condition = convertConditions(group1)
-        return `\nif (${condition}) {\n`;
-    });
-    result = result.replace(/end if/g, "\n}\n");
-    result = result.replace(/else(?!{)/g, "\n}\nelse {\n");
-
-    return result;
+function convertSwitchStatements(input: string) {
+  return input
+    .replace(/select case +(.*)/gi, "\nswitch ($1) {\n")
+    .replace(/end select/gi, "\n}\n")
+    .replace(/\sCase\s+else/gi, " default:")
+    .replace(/\sCase\s+(\"\w+"|\w+)/gi, " case $1:");
 }
 
-export function convertSwitchStatements(input: string) {
-    let result = input.replace(/select case +(.*)/g, "\nswitch ($1) {\n");
-    result = result.replace(/end select/g, "\n}\n");
-
-    return result;
+function convertAccessor(text: string, inClass: boolean) {
+  if (inClass) {
+    return "\t" + (text?.toLowerCase() || "");
+  }
+  return "";
 }
 
-export function convertFunctions(input: string) {
-    let result = input.replace(/function +(.*)\((.*)\)/g, "\n$1 = ($2) => {\n");
-    result = result.replace(/end function/g, "\n}\n");
+function convertFunctions(input: string, inClass: boolean) {
+  const replaceFn = (
+    input: string,
+    accessor: string,
+    type: string,
+    name: string,
+    params: string
+  ) => {
+    return `\n${convertAccessor(accessor, inClass)}${
+      inClass ? "" : "function "
+    }${name}(${params}) {\n`;
+  };
 
-    return result;
+  let result = input.replace(
+    /((Private|Public)\s+?)?(?:function|sub)\s+(\w+)\((.*?)\)/gi,
+    replaceFn
+  );
+  result = result.replace(
+    /end\s+(?:function|sub)/gi,
+    `\n${inClass ? "\t" : ""}}\n`
+  );
+
+  return result;
 }
 
-export function convertForStatements(input: string) {
-    let result = input.replace(/for +(.*to.*)/g, "\nfor ($1) {\n");
-    result = result.replace(/^ *next *$/gm, "}\n");
-
-    return result;
+function convertPropertyType(text: string) {
+  if (text.toLowerCase() === "let") return "set";
+  return text.toLowerCase();
 }
 
-export function convertConditions(input: string) {
-    let result = input.replace(/ +and +/g, " && ");
-    result = result.replace(/ +or +/g, " || ");
-    result = result.replace(/ +<> +/g, " !== ");
-    result = result.replace(/ += +/g, " === ");
+function convertProperties(input: string) {
+  const replaceFn = (
+    input: string,
+    accessor: string,
+    type: string,
+    name: string,
+    params: string
+  ) => {
+    return `\n${convertAccessor(accessor, true)} ${convertPropertyType(
+      type
+    )}${upperCaseFirst(name)}${params ?? "()"} {\n`;
+  };
 
-    return result;
+  let result = input.replace(
+    /(Private|Public)?\s*Property\s+(Get|Set|Let)\s+(\w+)(\(.*?\))?\n/gi,
+    replaceFn
+  );
+
+  result = result.replace(/end\s+(?:Property)/gi, "\n\t}\n");
+
+  return result;
 }
 
-export function convertLoops(input: string) {
-    let result = input.replace(/do until +(.*)/g, (input, group1) => {
-        const condition = convertConditions(group1);
-        return `\nwhile (!(${condition})) {\n`;
-    });
-    result = result.replace(/^ *loop *$/gm, "}\n");
+function convertClassFields(input: string) {
+  //console.log("fields: " + input);
+  const replaceFn = (input: string, accessor: string, names: string) => {
+    return names
+      .replace(/\s+/, "")
+      .split(",")
+      .map((name) => `\t${accessor?.toLowerCase() || ""} ${name}`)
+      .join("\n");
+  };
 
-    return result;
+  let result = input.replace(
+    /\s*(Private|Public)\s+([\s\w,]+?\n)/gi,
+    replaceFn
+  );
+  return result;
 }
 
-export function convertSplit(input: string) {
-    const result = input.replace(/Split\((.+?),(".+?")\)\s/g, `$1.split($2);`);
-
-    return result;
+function convertVariables(input: string) {
+  let result = input.replace(/\s(?:Dim)\s+(\w+)\s*:\s*(Set)?\s/gi, "let ");
+  result = result.replace(/\sDim\s/gi, " let ");
+  result = result.replace(/\sSet\s/gi, "");
+  result = result.replace(/(\s|=)Me\s/gi, "$1this");
+  result = result.replace(/\sNew\s+(\w+)\s*\n/gi, " new $1()\n");
+  result = result.replace(/\sNew\s/gi, " new ");
+  result = result.replace(/\Call\s/gi, " ");
+  return result;
 }
 
-export function convertPRec(input: string) {
-    const result = input.replace(/(p_rec\("\S+?"\))/g, "$1.Value");
+function convertForStatements(input: string) {
+  let result = input.replace(/for +(.*to.*)/gi, "\nfor ($1) {\n");
+  result = result.replace(/^\s*next\s*$/gim, "}\n");
 
-    return result;
+  return result;
 }
 
-export function convertPLan(input: string) {
-    const result = input.replace(/(l_\S+?)\(p_lan\)/g, "$1[p_lan]");
+function convertConditions(input: string) {
+  let result = input.replace(/ +and +/gi, " && ");
+  result = result.replace(/ +or +/gi, " || ");
+  result = result.replace(/ +<> +/gi, " !== ");
+  result = result.replace(/ += +/gi, " === ");
 
-    return result;
+  return result;
+}
+
+function convertLoops(input: string) {
+  let result = input.replace(/do until +(.*)/gi, (input, group1) => {
+    const condition = convertConditions(group1);
+    return `\nwhile (!(${condition})) {\n`;
+  });
+  result = result.replace(/^\s*loop\s*$/gm, "}\n");
+
+  return result;
+}
+
+function convertMany(input: string) {
+  return input
+    .replace(/_\s*\n/g, "")
+    .replace(/ByRef/gi, "/*ByRef*/")
+    .replace(/Class_Initialize/gi, "ctor")
+    .replace(/(\s|=)Nothing(\s*?\n)/gi, "$1undefined$2");
+}
+
+function convertSplit(input: string) {
+  const result = input.replace(/Split\((.+?),(".+?")\)\s/gi, `$1.split($2);`);
+  return result;
+}
+
+function convertPRec(input: string) {
+  const result = input.replace(/(p_rec\("\S+?"\))/gi, "$1.Value");
+  return result;
+}
+
+function convertPLan(input: string) {
+  const result = input.replace(/(l_\S+?)\(p_lan\)/gi, "$1[p_lan]");
+  return result;
+}
+
+function upperCaseFirst(input: string) {
+  return input.charAt(0).toUpperCase() + input.slice(1);
 }
