1a7fe9f5e53aea36a3c70dec53b2243fb3d60a25
immament <immament@gmail.com>
2021-11-11 18:34:55 +0000

    changed error msg

--- source/cli.ts
+++ source/cli.ts
@@ -7,5 +7,5 @@ if (existsSync(file)) {
   console.log(`Processing "${file}"...`);
   convertFile(file);
 } else {
-  console.error(`"${file}" not found! ${process.cwd()}`);
+  console.error(`File "${file}" not found!`);
 }
